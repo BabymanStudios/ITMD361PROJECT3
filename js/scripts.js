fe97795110eb7c3738d9c2b2165f05665fd72e83
BabymanStudios <lukepeters771@gmail.com>
2024-11-13 08:59:06 -0600

    Colors

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,9 @@
-// JavaScript for Image Slider
 let currentIndex = 0;
 const images = document.querySelectorAll('.slider img');
 const totalImages = images.length;
 
-// Initialize the first image as visible
 images[currentIndex].classList.add('active');
 
-// Show the image based on the current index
 function showImage(index) {
   images.forEach((img, i) => {
     img.classList.remove('active');
@@ -26,19 +23,18 @@ document.getElementById('prev').addEventListener('click', () => {
   showImage(currentIndex);
 });
 
-// Google Maps Initialization
 function initMap() {
   const hilo = { lat: 19.7297, lng: -155.0907 };
   const map = new google.maps.Map(document.getElementById('map'), {
     center: hilo,
     zoom: 12,
     styles: [
-      { elementType: "geometry", stylers: [{ color: "#ebe3cd" }] },
-      { elementType: "labels.text.fill", stylers: [{ color: "#523735" }] },
-      { elementType: "labels.text.stroke", stylers: [{ color: "#f5f1e6" }] }
+      { elementType: "geometry", stylers: [{ color: "#515A47" }] },
+      { elementType: "labels.text.fill", stylers: [{ color: "#D7BE82" }] },
+      { elementType: "labels.text.stroke", stylers: [{ color: "#7A4419" }] }
     ]
   });
-  new google.maps.Marker({
+  new google.maps.marker.AdvancedMarkerElement({
     position: hilo,
     map: map,
     title: 'Hilo, Hawaii',
